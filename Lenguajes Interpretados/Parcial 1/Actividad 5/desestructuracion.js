59478baaa300935f5fde82135cbccf3f2a2ce6fc
Juan Fernando Aispuro <Juanfernand2004@hotmail.com>
2023-08-28 14:19:22 -0600

    Update

---
+++ Lenguajes Interpretados/Parcial 1/Actividad 5/desestructuracion.js
@@ -0,0 +1,11 @@
+const personajes =  
+  [  
+    { id: 1, nombre: 'Solid Snake',   franquicia: 'MetalGear', icono:'📦' },
+    { id: 2, nombre: 'Master Chief',  franquicia: 'Halo',      icono:'🟢' },
+    { id: 3, nombre: 'Raiden',        franquicia: 'MetalGear', icono:'📦'},
+    { id: 4, nombre: 'Big Boss',      franquicia: 'MetalGear', icono:'📦'},
+    { id: 5, nombre: 'Cortana',       franquicia: 'Halo',      icono:'🟢'},
+    { id: 6, nombre: 'Inquisidor',    franquicia: 'Halo',      icono:'🟢'}
+  ]
+
+  
