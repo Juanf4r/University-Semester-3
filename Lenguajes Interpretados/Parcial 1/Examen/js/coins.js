3fe8ac8ec684753652fa044230dad871c944b687
Juan Fernando Aispuro <Juanfernand2004@hotmail.com>
2023-09-08 20:42:14 -0600

    Update

--- Lenguajes Interpretados/Parcial 1/Examen/js/coins.js
+++ Lenguajes Interpretados/Parcial 1/Examen/js/coins.js
@@ -1,205 +1,96 @@
+const player = document.getElementById('player');
+const scoreDisplay = document.getElementById('score');
+const highscoreDisplay = document.getElementById('highscore');
+const startButton = document.getElementById('start-button');
+const resetButton = document.getElementById('reset-button');
 
+let score = 0;
+let highscore = localStorage.getItem('highscore') || 0;
+let gameTimer;
+let gameDuration = 10; // Duración del juego en segundos
 
-var canvas = $("<canvas id='canvas' width='320' height='240'></canvas>");
-
-var mousex = 160,
-    mousey = 120;
-
-var finished = false;
-
-var score = 0,
-    coinslost = 0;
-
-canvas.appendTo('#center');
-
-var ctx = canvas.get(0).getContext("2d");
-
-var keydown = [];
-
-for(var i=0; i<128; i++)
-{
-  keydown[i] = false;
+function updateScore() {
+    scoreDisplay.textContent = score;
+    highscoreDisplay.textContent = highscore;
 }
 
-setInterval(function(){
-  update();
-  draw();
-}, 1000/60);
-
-setInterval(function(){
-  if(Math.random()>0.7)
-  {
-  addCoin();
-  }
-}, 1000/30);
-
-var player = {
-  x: 32,
-  y: 120,
-  width: 32,
-  height: 32,
-  yspeed: 0
-};
-
-var gravity = .5;
-
-function draw()
-{
-  if (finished==false)
-  {
-  ctx.clearRect(0,0,320,240);
-  
-  coins.forEach(function(coin){
-    if (coin.active)
-    {
-    coin.draw();
-    }
-  });
-  
-  ctx.fillStyle = "red";
-  ctx.fillRect(player.x-(player.width/2), player.y-(player.height/2), player.width, player.height);
-  
-  ctx.fillStyle = "black";
-  ctx.fillText("Score: " + score, 2, 10);
-  ctx.fillText("Coins Lost: " + coinslost, 2, 22);
-  }
-  else
-  {
-    ctx.clearRect(0,0,320,240);
-    ctx.fillText("You won with " + finishedcoins + " coins lost!", 80, 120);
-  }
-  
+function randomPosition() {
+    return Math.random() * (400 - 20);
 }
 
-var finishedcoins;
-
-function update()
-{
-  if (finished)
-  {
-    exit;
-  }
-  if (score>=100)
-  {
-    finished = true;
-    finishedcoins = coinslost;
-  }
-  gravity+=.5;
-  player.y += gravity;
-  player.y -= player.yspeed;
-  
-  if (player.y>240-16)
-  {
-    gravity = 0;
-    player.yspeed = 0;
-    player.y=240-16;
-  }
-  
-  coins.forEach(function(coin){
-    if(coin.active)
-    {
-    coin.update();
+function movePlayer(event) {
+    if (event.key === 'ArrowLeft') {
+        let left = parseFloat(player.style.left) || 0;
+        left -= 10;
+        left = Math.max(left, 0);
+        player.style.left = `${left}px`;
+    } else if (event.key === 'ArrowRight') {
+        let left = parseFloat(player.style.left) || 0;
+        left += 10;
+        left = Math.min(left, 360);
+        player.style.left = `${left}px`;
     }
-  });
-  
-  if (keydown[32])
-  {
-    if (gravity==0)
-    {
-      player.yspeed = 10;
-    }
-  }
-  
 }
 
-var coins = [];
+function startGame() {
+    score = 0;
+    updateScore();
+    resetButton.disabled = true;
+    startButton.disabled = true;
+    player.style.display = 'block';
 
-function addCoin()
-{
-  var coin = {
-    active: true,
-    x: Math.random()*320,
-    y: -16,
-    width: 8,
-    height: 16,
-    gravity: 0.5,
-    draw: function(){
-     
-      ctx.fillStyle = "yellow";
-      ctx.fillRect(this.x, this.y, this.width, this.height);
-      
-    },
-    
-    update: function(){
-     
-      this.gravity+=.5;
-      this.y+=this.gravity;
-      
-      if (this.gravity>3)
-      {
-        this.gravity = 3;
-      }
-      
-      if(collides(this,player))
-      {
-        score++;
-        this.active = false;
-      }
-      
-      if(this.y>240)
-      {
-        this.active = false;
-        coinslost++;
-      }
-      
-    }
-  };
-  
-  coins.push(coin);
-  
+    gameTimer = setInterval(() => {
+        const coin = document.createElement('div');
+        coin.className = 'coin';
+        coin.style.left = `${randomPosition()}px`;
+        document.getElementById('game-container').appendChild(coin);
+
+        const coins = document.getElementsByClassName('coin');
+
+        for (let i = 0; i < coins.length; i++) {
+            const coin = coins[i];
+            let top = parseFloat(coin.style.top) || 0;
+            if (top < 400) {
+                top += 2; // Velocidad de caída de la moneda
+                coin.style.top = `${top}px`;
+
+                // Verificar si el jugador atrapa la moneda
+                if (top > 380 && Math.abs(parseFloat(coin.style.left) - parseFloat(player.style.left)) < 20) {
+                    coin.remove();
+                    score++;
+                    updateScore();
+                }
+            } else {
+                coin.remove();
+            }
+        }
+
+        gameDuration--;
+        if (gameDuration <= 0) {
+            clearInterval(gameTimer);
+            player.style.display = 'none';
+            resetButton.disabled = false;
+            startButton.disabled = false;
+            if (score > highscore) {
+                highscore = score;
+                localStorage.setItem('highscore', highscore);
+            }
+            updateScore();
+        }
+    }, 1000 / 60);
 }
 
-$(document).keypress(function(event){
-  keydown[event.which] = true;
-});
-
-$(document).keyup(function(event){
-  keydown[event.which] = false;
-});
-
-function collides(a, b) {
-  
-  if(b==player)
-  {
-    b = {
-
-      x: player.x-16,
-      y: player.y-16,
-      width: player.width,
-      height: player.height
-    }
-  }
-  
-  return a.x < b.x + b.width &&
-         a.x + a.width > b.x &&
-         a.y < b.y + b.height &&
-         a.y + a.height > b.y;
+function resetGame() {
+    gameDuration = 10;
+    clearInterval(gameTimer);
+    player.style.display = 'none';
+    resetButton.disabled = true;
+    startButton.disabled = false;
+    document.querySelectorAll('.coin').forEach((coin) => coin.remove());
+    updateScore();
 }
 
-$('#canvas').mousedown(function(e){
-  if (gravity==0)
-    {
-      player.yspeed = 12;
-    }
-  
-  addCoin();
-});
-
-$("#canvas").mousemove(function(e){
-
-  mousex = e.pageX-this.offsetLeft;  
-  mousey = e.pageY-this.offsetTop;
-  
-  player.x = e.pageX-this.offsetLeft;
-  
-});
+document.addEventListener('keydown', movePlayer);
+startButton.addEventListener('click', startGame);
+resetButton.addEventListener('click', resetGame);
+updateScore();
