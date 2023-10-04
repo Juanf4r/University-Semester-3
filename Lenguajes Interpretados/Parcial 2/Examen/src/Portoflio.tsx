67df5cc8793bebe9a045b63643e4761a8f5b6823
Juan Fernando Aispuro <Juanfernand2004@hotmail.com>
2023-10-04 13:58:26 -0600

    Update

---
+++ Lenguajes Interpretados/Parcial 2/Examen/src/Portoflio.tsx
@@ -0,0 +1,16 @@
+import './css/Style.css' 
+import './css/normalize.css' 
+import './css/StyleSwitcher.css' 
+import './css/Skins/Color-1.css' 
+import './css/Skins/Color-2.css' 
+import './css/Skins/Color-3.css' 
+import './css/Skins/Color-4.css' 
+import './css/Skins/Color-5.css' 
+
+function Portoflio() {
+  return (
+    <div>Portoflio</div>
+  )
+}
+
+export default Portoflio
