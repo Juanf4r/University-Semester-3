150180a02b46301c62012cd2061c75f9995072da
Juan Fernando Aispuro <Juanfernand2004@hotmail.com>
2023-10-14 00:57:07 -0600

    Update

--- Lenguajes Interpretados/Parcial 3/Actividad1/src/App.tsx
+++ Lenguajes Interpretados/Parcial 3/Actividad1/src/App.tsx
@@ -1,122 +1,78 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 function doThreeJS(){
-
   const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
 
-  //Color fondo
   scene.background = new THREE.Color(0.25,0.6,0.95);
 
-  //Luz ambiental
-  const ambientLight = new THREE.AmbientLight(0xe0e0e0,1);
+  const ambientLight = new THREE.AmbientLight(0xe0e0e0, 1);
   scene.add(ambientLight);
   
-  //Luz direccional
   const light = new THREE.DirectionalLight(0xffffff,0.6);
   light.position.set(0,4,2);
   scene.add(light);
-  
-  const renderer = new THREE.WebGLRenderer();
-  //renderer.toneMapping = THREE.ACESFilmicToneMapping; //opciones aestethic
-  //renderer.outputColorSpace = THREE.SRGBColorSpace; //opciones aestethic
-  //renderer.setPixelRatio(window.devicePixelRatio); //opciones aestethic
-  renderer.setSize( window.innerWidth, window.innerHeight );
 
-  const controls = new OrbitControls( camera, renderer.domElement );
+  const loader = new GLTFLoader();
+  let model: THREE.Object3D;
 
-  document.body.appendChild( renderer.domElement );
+  loader.load('models/spaceship.gltf', (gltf) => {
+    model = gltf.scene;
+    model.scale.set(0.1, 0.1, 0.1);
+    scene.add(model);
 
-  const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-  const material = new THREE.MeshPhongMaterial( { color: 0xffffff } );  
-  const cube = new THREE.Mesh( geometry, material );
-  scene.add( cube );
+    const speed = 0.005;
+    let t = 0;
+    let direction = 1;
 
-  camera.position.z = 5;
+    const animate = () => {
+      t += speed * direction;
 
-  const clock = new THREE.Clock();
-
-  
-    const gltfloader = new GLTFLoader();
-    let ship: THREE.Object3D<THREE.Object3DEventMap>;
-    let loaded = false;
-
-    gltfloader.load(
-      // resource URL
-      'models/spaceship.gltf',
-      // called when the resource is loaded
-      function ( gltf ) {
-    
-        ship = gltf.scene
-        scene.add( ship );
-        loaded = true;
-  
-    
-      },
-      // called while loading is progressing
-      function ( xhr ) {
-    
-        console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-    
-      },
-      // called when loading has errors
-      function ( error ) {
-    
-        console.log( 'An error happened' );
-    
+      if (t >= 1) 
+      {
+        direction = -1;
+      } 
+      else if (t <= 0) 
+      {
+        direction = 1;
       }
-    );
 
+      const position = new THREE.Vector3(
+        Math.sin(2 * Math.PI * t) * 2, 
+        Math.sin(4 * Math.PI * t) * 1, 
+        0 
+      );
 
-      cube.position.set(0,-5,5);
+      model.position.copy(position);
+      renderer.render(scene, camera);
+      requestAnimationFrame(animate);
+    };
 
+    camera.position.z = 5;
 
-    function animate() {
-      requestAnimationFrame( animate );
-  
-      const delta = clock.getDelta();
-      const time = clock.getElapsedTime();
+    animate();
+  });
 
-      cube.rotation.x += 2 * delta;
-      cube.rotation.y += 2 * delta;
-
-      cube.position.set(Math.sin(time)*5,Math.cos(time)*5,1);
-
-    if(loaded)
-    {
-      ship.rotateY(delta);
-      ship.translateZ(delta *10);
-    }
-
-    // required if controls.enableDamping or controls.autoRotate are set to true
-	  controls.update();
-    renderer.render( scene, camera );
-  }
-
-  window.addEventListener( 'resize', onWindowResize, false );
-  
-  function onWindowResize(){ //funcion para redimensionar ventana si el usuario le anda moviendo
-    camera.aspect = window.innerWidth / window.innerHeight;
+  window.addEventListener('resize', () => {
+    const newWidth = window.innerWidth;
+    const newHeight = window.innerHeight;
+    camera.aspect = newWidth / newHeight;
     camera.updateProjectionMatrix();
-
-    renderer.setSize( window.innerWidth, window.innerHeight );
-  }
-  
-  animate(); //Iniciamos el loop
+    renderer.setSize(newWidth, newHeight);
+  });
 }
-
 
 const App = () => {
 
   return (
     <>
-      <div id="info">Buenas</div>
       {doThreeJS()}
     </>
   )
 }
 
 export default App
-
