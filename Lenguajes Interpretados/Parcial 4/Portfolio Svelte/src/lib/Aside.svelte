bbf7a09e9e7e3c3002f58661e9885212c9a72a68
Juan Fernando Aispuro <Juanfernand2004@hotmail.com>
2023-11-24 12:12:31 -0600

    a

--- Lenguajes Interpretados/Parcial 4/Portfolio Svelte/src/lib/Aside.svelte
+++ Lenguajes Interpretados/Parcial 4/Portfolio Svelte/src/lib/Aside.svelte
@@ -1,6 +1,6 @@
 <script>
     import Section from "./Section.svelte";
-    import {Router, Route, Link} from "svelte-routing";
+    //import {Router, Route, Link} from "svelte-routing";
     import Home from "./Section.svelte";
     import About from "./AboutSection.svelte";
     import Services from "./Services.svelte";
@@ -17,21 +17,21 @@
     </div>
     <ul class="nav">
         <nav>
-            <li><Link to ="/" class="active"><i class="fa fa-home"></i> Home</Link></li>
+            <!-- <li><Link to ="/" class="active"><i class="fa fa-home"></i> Home</Link></li>
             <li><Link to ="/about"><i class="fa fa-user"></i> About</Link></li>
             <li><Link to ="/skills"><i class="fa fa-list"></i>Skills</Link></li>
             <li><Link to ="/portfolio"><i class="fa fa-briefcase"></i>Portfolio</Link></li>
-            <li><Link to ="/contact"><i class="fa fa-comments"></i>Contact</Link></li>
+            <li><Link to ="/contact"><i class="fa fa-comments"></i>Contact</Link></li> -->
         </nav>
     </ul>
 
-    <Router>
+    <!-- <Router>
         <Route path="/" component={Home} />
         <Route path="/about" component={About} />
         <Route path="/skills" component={Services} />
         <Route path="/portfolio" component={Portfolio} />
         <Route path="/contact" component={Contact} />
-    </Router>
+    </Router> -->
 
 
 </div>
